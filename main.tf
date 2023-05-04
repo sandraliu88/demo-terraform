2677af85d6cfeab6a80016c9cedd29ba626ee84e
Sandra Liu <sandraliu.m@gmail.com>
2023-05-04 13:09:34 -0700

    new app

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-east-2"
-}
+## provider "aws" {
+##  region  = "us-east-2"
+## }
 
 resource "aws_instance" "sandra_demo_instaces" {
   ami           = "var.ami-id"
