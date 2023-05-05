6c867d60246034eeea1bafa4548bff6f0292f83c
Sandra Liu <sandraliu.m@gmail.com>
2023-05-04 22:13:27 -0700

    new instance

--- main.tf
+++ main.tf
@@ -14,10 +14,12 @@
 # data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
 
 resource "aws_instance" "app_server" {
-ami           = "data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id"
+ami           = "ami-097304bf0639dd83a"
 instance_type = "t2.micro"
  tags          = {
    Name = "learn_hcp_packer"
+   purpose ="demo"
+   terraform ="yes"
    }
   }
 
@@ -36,14 +38,14 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "sandra_demo_instaces" {
-  ami           = "var.ami-id"
-  instance_type = "t2.micro"
+## resource "aws_instance" "sandra_demo_instaces" {
+##  ami           = "var.ami-id"
+##  instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-    ttl = "500"
-    purpose = "demo"
-    terraform = "yes"
-  }
-}
+## tags = {
+##    Name = "ExampleAppServerInstance"
+##    ttl = "500"
+##    purpose = "demo"
+##    terraform = "yes"
+##  }
+## }
