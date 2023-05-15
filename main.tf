9f173520536e822d85e3bdd3da627ff9e195dda7
Sandra Liu <sandraliu.m@gmail.com>
2023-05-15 15:06:55 -0700

    Standing up 2 instances to the main branch

--- main.tf
+++ main.tf
@@ -45,8 +45,13 @@ provider "aws" {
  resource "aws_instance" "sandra_demo_instances" {
   ami           = data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
   instance_type = "t2.micro"
+<<<<<<< HEAD
   count = 3
 
+=======
+  count = 2
+  
+>>>>>>> a420e86 (Standing up 2 instances)
  tags = {
     Name = "ExampleAppServerInstance"
     ttl = "500"
