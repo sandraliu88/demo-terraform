419a0c3e3cf829d0168c311f617baaada8376c8e
Sandra Liu <sandraliu.m@gmail.com>
2023-05-22 23:21:39 -0500

    two new instances

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ provider "aws" {
   ami           = data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
   instance_type = "t2.micro"
 
-  count = 2
+  count = 22
 
  tags = {
     Name = "webapp"
