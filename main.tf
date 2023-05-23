15bde33876f911089e9facadc8bf4c1fd045dd81
Sandra Liu <sandraliu.m@gmail.com>
2023-05-22 23:11:36 -0500

    add webapp

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@
  #   value         = hcp_packer_image.learner-packer-ubuntu
  # }
 
-# Then replace your existing references with
-# data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
-
 resource "aws_instance" "app_server" {
 ami           = data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
 instance_type = "t2.micro"
@@ -26,21 +23,6 @@ instance_type = "t2.micro"
    terraform ="yes"
    }
   }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-2"
-}
 
  resource "aws_instance" "sandra_demo_instances" {
   ami           = data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
