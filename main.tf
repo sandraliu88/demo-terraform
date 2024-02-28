d0ddf14f37acc3fc275a27504b7c03f8fcae50c3
Sandra Liu <sandraliu.m@gmail.com>
2024-02-28 11:24:39 -0800

    more instances

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ provider "aws" {
 resource "aws_instance" "sandra_demo_instances" {
   ami           = data.hcp_packer_image.learn-packer-ubuntu.cloud_image_id
   instance_type = "t2.micro"
-  count         = 3
+  count         = 22
 
   tags = {
       Name = "mywebapp"
