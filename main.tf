d3bf10ad239b55e0dedc7d97d908b180bd10cd4a
sandraliu88 <69475214+sandraliu88@users.noreply.github.com>
2024-02-28 08:05:31 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "sandra_demo_instances" {
   count         = 2
 
   tags = {
-      Name = "synopsysdemo"
+      Name = "genericdemo"
       purpose = "demo"
       ttl = "500"
       se-region = "west"
