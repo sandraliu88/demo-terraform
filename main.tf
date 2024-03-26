1f83deb369075cfc642f3270e3a5e8a04cd5dd25
Sandra Liu <sandraliu.m@gmail.com>
2024-03-26 13:07:54 -0700

    cloudera demo

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.38.0"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
+
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "sandra_demo_instances" {
